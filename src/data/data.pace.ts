58e0695785271e512bdd939b59d4122c7b5149a1
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-05-19 20:38:55 +0200

    chore: perhaps pace should derive from duration

--- src/data/data.pace.ts
+++ src/data/data.pace.ts
@@ -1,6 +1,6 @@
-import {DataNumber} from './data.number';
+import {DataDuration} from './data.duration';
 
-export class DataPace extends DataNumber {
+export class DataPace extends DataDuration {
   static className = 'DataPace';
   static type = 'Pace';
   static unit = 'm/km';

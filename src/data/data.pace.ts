fc6d0fbacfc6cd6b86f07d49f3b7a9fe7ada6cb4
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-05-20 12:56:37 +0200

    chore: fix units for pace

--- src/data/data.pace.ts
+++ src/data/data.pace.ts
@@ -1,13 +1,12 @@
 import {DataDuration} from './data.duration';
 
 export class DataPace extends DataDuration {
-
   static className = 'DataPace';
   static type = 'Pace';
   static unit = 'm/km';
 
   getDisplayValue(): string | string | string {
-    const d = this.getValue();
+    const d = this.value;
     const h = Math.floor(d / 3600);
     const m = Math.floor(d % 3600 / 60);
     const s = Math.floor(d % 3600 % 60);
@@ -18,6 +17,10 @@ export class DataPace extends DataDuration {
     } else {
       return ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2);
     }
+  }
+
+  getValue(): number {
+    return super.getValue() / 60;
   }
 
   getDisplayUnit(): string {

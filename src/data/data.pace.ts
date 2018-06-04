4fd5f277c1dce1325cc1d7c3380fc52e7c99de03
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-06-04 23:31:26 +0200

    chore(date): fix pace display issue

--- src/data/data.pace.ts
+++ src/data/data.pace.ts
@@ -5,13 +5,13 @@ export class DataPace extends DataDuration {
   static type = 'Pace';
   static unit = 'm/km';
 
-  getDisplayValue(): string | string | string {
+  getDisplayValue(): string{
     const d = this.getValue();
     const h = Math.floor(d / 3600);
     const m = Math.floor(d % 3600 / 60);
     const s = Math.floor(d % 3600 % 60);
     if (!m && !h) {
-      return ('0' + s).slice(-2);
+      return '00:' + ('0' + s).slice(-2);
     } else if (!h) {
       return ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2);
     } else {

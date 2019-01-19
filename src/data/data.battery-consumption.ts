d71e8d2de13caa78dba2005b2d8432bd70eaacd6
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-01-19 12:24:07 +0100

    chore(battery-consumption): fix display value

--- src/data/data.battery-consumption.ts
+++ src/data/data.battery-consumption.ts
@@ -3,4 +3,8 @@ import {DataNumber} from './data.number';
 export class DataBatteryConsumption extends DataNumber {
   static type = 'Battery Consumption';
   static unit = '%';
+
+  getDisplayValue(): number | string {
+    return this.getValue().toFixed(2);
+  }
 }

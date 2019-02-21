ca9320c643ce7d9de5a513180fe98d0ed9e4e284
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-02-21 22:25:18 +0100

    chore: show units for duration

--- src/data/data.duration.ts
+++ src/data/data.duration.ts
@@ -17,8 +17,4 @@ export class DataDuration extends DataNumber {
       return ('0' + h).slice(-2) + 'h ' + ('0' + m).slice(-2) + 'm ' + ('0' + s).slice(-2) + 's';
     }
   }
-
-  getDisplayUnit() {
-    return '';
-  }
 }

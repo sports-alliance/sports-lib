df5a36c77768612eac3990ed77fbd7e243b028ba
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-03-13 18:55:35 +0100

    chore: compile lib

---
+++ lib/data/data.device-location.d.ts
@@ -0,0 +1,4 @@
+import { DataString } from './data.string';
+export declare class DataDeviceLocation extends DataString {
+    static type: string;
+}
---
+++ lib/data/data.pool-length.d.ts
@@ -0,0 +1,5 @@
+import { DataNumber } from './data.number';
+export declare class DataPoolLength extends DataNumber {
+    static type: string;
+    static unit: string;
+}

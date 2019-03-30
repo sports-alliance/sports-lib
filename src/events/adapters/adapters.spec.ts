9bd1408e4b5fc400bd20908d8ac044ed0288dfc4
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-03-30 19:57:28 +0100

    chore: improving tests

--- src/events/adapters/adapters.spec.ts
+++ src/events/adapters/adapters.spec.ts
@@ -4,12 +4,11 @@ import {EventImporterTCX} from "./importers/tcx/importer.tcx";
 import {Event} from "../event";
 
 const suuntoJSON = require('../../../samples/suunto/suunto.json');
-const garminTCX = require('../../../samples/tcx/garmin.tcx.json');
 
 import * as fs from 'fs';
-// var DOMParser = require('xmldom').DOMParser;
+var DOMParserCustom = require('xmldom').DOMParser;
 
-// const tcxString = fs.readFileSync(__dirname + '/Meylan_Aix.tcx', 'utf8');
+const tcxString = fs.readFileSync(__dirname + '/../../../samples/tcx/garmin.tcx', 'utf8');
 describe('EventAdapters', () => {
 
   beforeEach(() => {
@@ -18,7 +17,7 @@ describe('EventAdapters', () => {
 
   it('should import and export correctly from Suunto adapter', () => {
     // First get it from adapter 1
-    const event1 = EventImporterSuuntoJSON.getFromJSONString(JSON.stringify(suuntoJSON));
+    // const event1 = EventImporterSuuntoJSON.getFromJSONString(JSON.stringify(suuntoJSON));
     // const event2 = EventImporterJSON.getEventFromJSON(event1.toJSON());
 
     // event1.name = event2.name;
@@ -27,25 +26,22 @@ describe('EventAdapters', () => {
 
   it('should import and export correctly some tcx', () => {
     const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(garminTCX.tcx,"text/xml");
-    // const event = EventImporterTCX.getFromXML(xmlDoc).then((result) => {
-    //   debugger;
-    // });
+    const xmlDoc = parser.parseFromString(tcxString,"text/xml");
     expect.assertions(1);
     return expect(EventImporterTCX.getFromXML(xmlDoc)).resolves.toBeInstanceOf(Event);
     // console.log(event)
   });
 
-  it('should import and export correctly some tcx using DomParser', () => {
-  //   const parser = new DOMParser();
-  //   const domParser = new DOMParser();
+  // fit('should import and export correctly some tcx using DomParser', () => {
+  //   const parser = new DOMParserCustom();
   //
-  //   const xmlDoc = parser.parseFromString(garminTCX.tcx,"text/xml");
-  //   // const event = EventImporterTCX.getFromXML(xmlDoc).then((result) => {
-  //   //   debugger;
-  //   // });
+  //   const xmlDoc = parser.parseFromString(tcxString,"'application/xml'");
+  //   const promist = EventImporterTCX.getFromXML(xmlDoc).catch((res)=>{
+  //     const b=1
+  //     debugger;
+  //   })
   //   expect.assertions(1);
   //   return expect(EventImporterTCX.getFromXML(xmlDoc)).resolves.toBeInstanceOf(Event);
   //   // console.log(event)
-  });
+  // });
 });

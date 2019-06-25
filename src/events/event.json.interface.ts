23d046e41c65e6849c1dc2954964175fdd58d098
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-06-25 20:25:46 +0200

    chore: decouple meta

--- src/events/event.json.interface.ts
+++ src/events/event.json.interface.ts
@@ -1,6 +1,5 @@
 import {DataJSONInterface} from '../data/data.json.interface';
 import {Privacy} from '../privacy/privacy.class.interface';
-import {MetaDataJsonInterface} from '../meta-data/meta-data.json.interface';
 
 export interface EventJSONInterface {
   name: string,

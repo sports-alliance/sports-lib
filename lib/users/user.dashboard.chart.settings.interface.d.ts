1e0dcc92683461452e3f2f07c70831c5d70f3d77
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-06-28 20:30:01 +0200

    chore: add value type

--- lib/users/user.dashboard.chart.settings.interface.d.ts
+++ lib/users/user.dashboard.chart.settings.interface.d.ts
@@ -3,9 +3,16 @@ export interface UserDashboardChartSettingsInterface {
     order: number;
     type: ChartTypes;
     dataType: string;
+    valueType: ValueTypes;
 }
 export declare enum ChartTypes {
     Pie = "Pie",
     ColumnsHorizontal = "Columns Horizontal",
     PyramidsVertical = "Pyramids Vertical"
+}
+export declare enum ValueTypes {
+    Sum = "Sum",
+    Average = "Average",
+    Maximum = "Maximum",
+    Minimum = "Minimum"
 }
--- src/users/user.dashboard.chart.settings.interface.ts
+++ src/users/user.dashboard.chart.settings.interface.ts
@@ -3,10 +3,18 @@ export interface UserDashboardChartSettingsInterface {
   order: number,
   type: ChartTypes,
   dataType: string,
+  valueType: ValueTypes
 }
 
 export enum ChartTypes {
   Pie = 'Pie',
   ColumnsHorizontal = 'Columns Horizontal',
   PyramidsVertical = 'Pyramids Vertical',
+}
+
+export enum ValueTypes {
+  Sum = 'Sum',
+  Average = 'Average',
+  Maximum = 'Maximum',
+  Minimum = 'Minimum',
 }

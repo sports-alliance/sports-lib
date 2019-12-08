9930f2c95a16530845081904a9093034e694a4a8
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-12-08 11:03:36 +0100

    chore: fixes on display type

--- src/data/data.total-training-effect.ts
+++ src/data/data.total-training-effect.ts
@@ -2,6 +2,8 @@ import {DataNumber} from './data.number';
 
 export class DataTotalTrainingEffect extends DataNumber {
   static type = 'Total Training effect';
+  static displayType = 'Total Training Effect';
+
   getDisplayValue() {
     return this.value.toFixed(1)
   }

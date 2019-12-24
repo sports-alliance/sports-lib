220215f1fa76af7e44c76e771bb9bfa0b6d41380
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-12-24 21:12:23 +0100

    chore: make optional

--- src/streams/stream.interface.ts
+++ src/streams/stream.interface.ts
@@ -4,8 +4,8 @@ export interface StreamInterface extends SerializableClassInterface {
   type: string;
   data: (number|null)[];
   getNumericData(): (number)[];
-  getStreamDataByTime(startDate: Date, filterNull: boolean, filterInfinity: boolean): StreamDataItem[]
-  getStreamDataByDuration(offset: number, filterNull: boolean, filterInfinity: boolean): StreamDataItem[]
+  getStreamDataByTime(startDate: Date, filterNull?: boolean, filterInfinity?: boolean): StreamDataItem[]
+  getStreamDataByDuration(offset: number, filterNull?: boolean, filterInfinity?: boolean): StreamDataItem[]
   isUnitDerivedDataType(): boolean;
   toJSON(): StreamJSONInterface;
 }

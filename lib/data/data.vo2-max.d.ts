0ff0414e95c3272f8a54eb61dceff9ac249244fa
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-01-01 17:12:50 +0100

    chore: fix display value for vo2max

--- lib/data/data.vo2-max.d.ts
+++ lib/data/data.vo2-max.d.ts
@@ -1,4 +1,5 @@
 import { DataNumber } from './data.number';
 export declare class DataVO2Max extends DataNumber {
     static type: string;
+    getDisplayValue(): string;
 }
--- src/data/data.vo2-max.ts
+++ src/data/data.vo2-max.ts
@@ -2,4 +2,8 @@ import {DataNumber} from './data.number';
 
 export class DataVO2Max extends DataNumber {
   static type = 'VO2 Max';
+
+  getDisplayValue(): string {
+    return this.getValue().toFixed(2)
+  }
 }

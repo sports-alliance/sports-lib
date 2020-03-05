922fa5ae0bb4ad7ba7dc25810a34986ba016062d
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-03-05 13:28:35 +0100

    chore: add tests for strava compliance

---
+++ integrationTests/strava-compliance.spec.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import { SportsLib } from '../src';
+import { EventInterface } from '../src/events/event.interface';
+import * as strava_3156040843 from './fixtures/strava_compliance/suunto_export/strava_3156040843.json';
+import { DataAltitude } from '../src/data/data.altitude';
+
+function clone(obj: any) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+describe('Strava data compliance', () => {
+
+  it('should match altitude', done => {
+
+    // Given
+    const path = __dirname + '/fixtures/strava_compliance/suunto_export/5e5fde38c2de24635a30d383.fit';
+    const buffer = fs.readFileSync(path);
+
+    const altitudeStream = clone(strava_3156040843.altitude);
+
+    // When
+    const eventInterfacePromise = SportsLib.importFromFit(buffer);
+
+    // Then
+    eventInterfacePromise.then((event: EventInterface) => {
+      expect(altitudeStream.length).toEqual(event.getFirstActivity().getStreamData(DataAltitude.type).length);
+      // @todo Thomas do you think that this precision change should happen library wise?
+      expect(altitudeStream).toEqual(event.getFirstActivity().getStreamData(DataAltitude.type).map(altitude => altitude === null ? null :  Math.round(altitude * 10) / 10));
+      done();
+    });
+  });
+});

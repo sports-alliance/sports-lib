13571404d4f45c7a4206e01242bc0cbafc99f9fd
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-03-05 19:24:09 +0100

    refactor: rename to show it's specific to eg suunto
    I only have suunto files to check and get data from the strava api more to be added

--- integrationTests/strava-compliance.spec.ts
+++ integrationTests/strava-compliance.spec.ts
@@ -8,6 +8,7 @@ import { DataCadence } from '../src/data/data.cadence';
 import { DataTemperature } from '../src/data/data.temperature';
 import { DataPower } from '../src/data/data.power';
 import { DataDistance } from '../src/data/data.distance';
+import { DataGrade } from '../src/data/data-grade';
 
 function clone(obj: any) {
   return JSON.parse(JSON.stringify(obj));
@@ -21,16 +22,16 @@ describe('Strava data compliance', () => {
     const path = __dirname + '/fixtures/strava_compliance/suunto_export/5e5fde38c2de24635a30d383.fit';
     const buffer = fs.readFileSync(path);
 
-    const altitudeStream = clone(strava_3156040843.altitude);
+    const stravaAltitudeStream = clone(strava_3156040843.altitude);
 
     // When
     const eventInterfacePromise = SportsLib.importFromFit(buffer);
 
     // Then
     eventInterfacePromise.then((event: EventInterface) => {
-      expect(altitudeStream.length).toEqual(event.getFirstActivity().getStreamData(DataAltitude.type).length);
+      expect(stravaAltitudeStream.length).toEqual(event.getFirstActivity().getStreamData(DataAltitude.type).length);
       // @todo Thomas do you think that this precision change should happen library wise?
-      expect(altitudeStream).toEqual(event.getFirstActivity().getStreamData(DataAltitude.type).map(value => value === null ? null :  Math.round(value * 10) / 10));
+      expect(stravaAltitudeStream).toEqual(event.getFirstActivity().getStreamData(DataAltitude.type).map(value => value === null ? null :  Math.round(value * 10) / 10));
       done();
     });
   });
@@ -41,15 +42,15 @@ describe('Strava data compliance', () => {
     const path = __dirname + '/fixtures/strava_compliance/suunto_export/5e5fde38c2de24635a30d383.fit';
     const buffer = fs.readFileSync(path);
 
-    const heartRateStream = clone(strava_3156040843.heartrate);
+    const straveHeartRateStream = clone(strava_3156040843.heartrate);
 
     // When
     const eventInterfacePromise = SportsLib.importFromFit(buffer);
 
     // Then
     eventInterfacePromise.then((event: EventInterface) => {
-      expect(heartRateStream.length).toEqual(event.getFirstActivity().getStreamData(DataHeartRate.type).length);
-      expect(heartRateStream).toEqual(event.getFirstActivity().getStreamData(DataHeartRate.type).map(value => value === null ? null :  Math.round(value * 10) / 10));
+      expect(straveHeartRateStream.length).toEqual(event.getFirstActivity().getStreamData(DataHeartRate.type).length);
+      expect(straveHeartRateStream).toEqual(event.getFirstActivity().getStreamData(DataHeartRate.type).map(value => value === null ? null :  Math.round(value * 10) / 10));
       done();
     });
   });
@@ -60,15 +61,15 @@ describe('Strava data compliance', () => {
     const path = __dirname + '/fixtures/strava_compliance/suunto_export/5e5fde38c2de24635a30d383.fit';
     const buffer = fs.readFileSync(path);
 
-    const cadenceStream = clone(strava_3156040843.cadence);
+    const stravaCadenceStream = clone(strava_3156040843.cadence);
 
     // When
     const eventInterfacePromise = SportsLib.importFromFit(buffer);
 
     // Then
     eventInterfacePromise.then((event: EventInterface) => {
-      expect(cadenceStream.length).toEqual(event.getFirstActivity().getStreamData(DataCadence.type).length);
-      expect(cadenceStream).toEqual(event.getFirstActivity().getStreamData(DataCadence.type).map(value => value === null ? null :  Math.round(value * 10) / 10));
+      expect(stravaCadenceStream.length).toEqual(event.getFirstActivity().getStreamData(DataCadence.type).length);
+      expect(stravaCadenceStream).toEqual(event.getFirstActivity().getStreamData(DataCadence.type).map(value => value === null ? null :  Math.round(value * 10) / 10));
       done();
     });
   });
@@ -79,15 +80,15 @@ describe('Strava data compliance', () => {
     const path = __dirname + '/fixtures/strava_compliance/suunto_export/5e5fde38c2de24635a30d383.fit';
     const buffer = fs.readFileSync(path);
 
-    const temperatureStream = clone(strava_3156040843.temp);
+    const stravaTemperatureStream = clone(strava_3156040843.temp);
 
     // When
     const eventInterfacePromise = SportsLib.importFromFit(buffer);
 
     // Then
     eventInterfacePromise.then((event: EventInterface) => {
-      expect(temperatureStream.length).toEqual(event.getFirstActivity().getStreamData(DataTemperature.type).length);
-      expect(temperatureStream).toEqual(event.getFirstActivity().getStreamData(DataTemperature.type).map(value => value === null ? null :  Math.round(value * 10) / 10));
+      expect(stravaTemperatureStream.length).toEqual(event.getFirstActivity().getStreamData(DataTemperature.type).length);
+      expect(stravaTemperatureStream).toEqual(event.getFirstActivity().getStreamData(DataTemperature.type).map(value => value === null ? null :  Math.round(value * 10) / 10));
       done();
     });
   });
@@ -98,39 +99,63 @@ describe('Strava data compliance', () => {
     const path = __dirname + '/fixtures/strava_compliance/suunto_export/5e5fde38c2de24635a30d383.fit';
     const buffer = fs.readFileSync(path);
 
-    const powerStream = clone(strava_3156040843.watts);
+    const stravaPowerStream = clone(strava_3156040843.watts);
 
     // When
     const eventInterfacePromise = SportsLib.importFromFit(buffer);
 
     // Then
     eventInterfacePromise.then((event: EventInterface) => {
-      expect(powerStream.length).toEqual(event.getFirstActivity().getStreamData(DataPower.type).length);
-      expect(powerStream).toEqual(event.getFirstActivity().getStreamData(DataPower.type).map(value => value === null ? null :  Math.round(value * 10) / 10));
+      expect(stravaPowerStream.length).toEqual(event.getFirstActivity().getStreamData(DataPower.type).length);
+      expect(stravaPowerStream).toEqual(event.getFirstActivity().getStreamData(DataPower.type).map(value => value === null ? null :  Math.round(value * 10) / 10));
       done();
     });
   });
 
-  it('should match distance', done => {
+  it('should match distance with 0.20% error max', done => {
 
     // Given
     const path = __dirname + '/fixtures/strava_compliance/suunto_export/5e5fde38c2de24635a30d383.fit';
     const buffer = fs.readFileSync(path);
     const tolerance = 0.20; // percent
 
-    const distanceStream = clone(strava_3156040843.distance);
+    const stravaDistanceStream = clone(strava_3156040843.distance);
 
     // When
     const eventInterfacePromise = SportsLib.importFromFit(buffer);
 
     // Then
     eventInterfacePromise.then((event: EventInterface) => {
-      expect(distanceStream.length).toEqual(event.getFirstActivity().getStreamData(DataDistance.type).length);
-      const importDistanceStreamData = event.getFirstActivity().getStreamData(DataDistance.type).map(value => value === null ? null :  Math.round(value * 10) / 10)
-      const commonCount = distanceStream
-        .filter((value: (number|null)) => importDistanceStreamData.indexOf(value) !== -1).length;
+      expect(stravaDistanceStream.length).toEqual(event.getFirstActivity().getStreamData(DataDistance.type).length);
+      const suuntoDistanceStreamData = event.getFirstActivity().getStreamData(DataDistance.type).map(value => value === null ? null :  Math.round(value * 10) / 10)
+      const commonCount = stravaDistanceStream
+        .filter((value: (number|null)) => suuntoDistanceStreamData.indexOf(value) !== -1).length;
       // We find the common then add the 1% tolerance and we check if its more than equal to the "strava" stream
-      expect(commonCount + Math.ceil((distanceStream.length * tolerance) / 100)).toBeGreaterThanOrEqual(distanceStream.length);
+      expect(commonCount + Math.ceil((stravaDistanceStream.length * tolerance) / 100)).toBeGreaterThanOrEqual(stravaDistanceStream.length);
+      done();
+    });
+  });
+
+  it('should match grade with 0.20% error max', done => {
+
+    // Given
+    const path = __dirname + '/fixtures/strava_compliance/suunto_export/5e5fde38c2de24635a30d383.fit';
+    const buffer = fs.readFileSync(path);
+    const tolerance = 0.20; // percent
+
+    const stravaGradeStream = clone(strava_3156040843.grade_smooth);
+
+    // When
+    const eventInterfacePromise = SportsLib.importFromFit(buffer);
+
+    // Then
+    eventInterfacePromise.then((event: EventInterface) => {
+      expect(stravaGradeStream.length).toEqual(event.getFirstActivity().getStreamData(DataGrade.type).length);
+      const suuntoGradeStreamData = event.getFirstActivity().getStreamData(DataGrade.type).map(value => value === null ? null :  Math.round(value * 10) / 10)
+      const commonCount = stravaGradeStream
+        .filter((value: (number|null)) => suuntoGradeStreamData.indexOf(value) !== -1).length;
+      // We find the common then add the 1% tolerance and we check if its more than equal to the "strava" stream
+      expect(commonCount + Math.ceil((stravaGradeStream.length * tolerance) / 100)).toBeGreaterThanOrEqual(stravaGradeStream.length);
       done();
     });
   });

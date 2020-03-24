b5cca91c5300a54b5ca1ffb7dc7ce36dd6325626
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-03-24 19:21:29 +0100

    fix: name

--- src/users/user.service.meta.interface.ts
+++ src/users/user.service.meta.interface.ts
@@ -1,5 +1,5 @@
 export interface UserServiceMetaInterface {
   didLastHistoryImport: number;
   processedActivitiesFromLastHistoryImport: number;
-  uploadedRoutes?: number
+  uploadedRoutesCount?: number
 }

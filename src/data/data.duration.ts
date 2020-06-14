c6f36912425202c05ab715f737069c417e007e01
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-06-14 18:23:39 +0200

    chore: support miliseconds

--- src/data/data.duration.ts
+++ src/data/data.duration.ts
@@ -5,29 +5,32 @@ export class DataDuration extends DataNumber {
   static unit = 's';
 
   /**
-   * Converts to hhh:mmm:ss
+   * Converts to hhh:mmm:ss:ms
    * @todo should adopt and round depending if needed to show seconds or not
    * @param showDays
    * @param showSeconds
+   * @param showMilliseconds
    */
-  getDisplayValue(showDays = false, showSeconds = true) {
+  getDisplayValue(showDays = false, showSeconds = true, showMilliseconds = false) {
     const seconds = this.getValue();
     const h = Math.floor(seconds / 3600);
     const d = Math.floor(h / 24);
     const m = Math.floor(seconds % 3600 / 60);
     const s = Math.floor(seconds % 3600 % 60);
+    const ms = (Math.round((seconds % 1) * 10) / 10).toString().substring(1);
+
     if (!m && !h) {
-      return showSeconds ? ('0' + s).slice(-2) + 's' : s + `s`;
+      return showSeconds ? ('0' + s).slice(-2) + (showMilliseconds ? ms : '')  + 's' : s + String(showMilliseconds ? ms : '') + `s`;
     } else if (!h) {
-      return ('0' + m).slice(-2) + 'm' + (showSeconds ? ' ' + ('0' + s).slice(-2) + 's' : ``);
+      return ('0' + m).slice(-2) + 'm' + (showSeconds ? ' ' + ('0' + s).slice(-2) + (showMilliseconds ? ms : '') + 's' : ``);
     } else {
       if (d) {
         if (showDays) {
-          return d + 'd ' + ('0' + (h - d * 24)).slice(-2) + 'h ' + ('0' + m).slice(-2) + 'm ' + (showSeconds ? ('0' + s).slice(-2) + 's' : ``);
+          return d + 'd ' + ('0' + (h - d * 24)).slice(-2) + 'h ' + ('0' + m).slice(-2) + 'm ' + (showSeconds ? ('0' + s).slice(-2) + (showMilliseconds ? ms : '') + 's' : ``);
         }
-        return h + 'h ' + ('0' + m).slice(-2) + 'm ' + (showSeconds ? ('0' + s).slice(-2) + 's' : ``);
+        return h + 'h ' + ('0' + m).slice(-2) + 'm ' + (showSeconds ? ('0' + s).slice(-2) + (showMilliseconds ? ms : '') + 's' : ``);
       } else {
-        return ('0' + h).slice(-2) + 'h ' + ('0' + m).slice(-2) + 'm ' + (showSeconds ? ('0' + s).slice(-2) + 's' : ``);
+        return ('0' + h).slice(-2) + 'h ' + ('0' + m).slice(-2) + 'm ' + (showSeconds ? ('0' + s).slice(-2) + (showMilliseconds ? ms : '') + 's' : ``);
       }
     }
   }

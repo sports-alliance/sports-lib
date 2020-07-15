642735929fa31c9d50ec08af3414c2af0bcaab45
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-07-15 21:32:58 +0200

    chore: round grade

--- src/data/data.grade.ts
+++ src/data/data.grade.ts
@@ -2,4 +2,8 @@ import { DataPercent } from './data.percent';
 
 export class DataGrade extends DataPercent {
   static type = 'Grade';
+
+  getDisplayValue(): number | string | string[] {
+    return Math.round(this.value);
+  }
 }

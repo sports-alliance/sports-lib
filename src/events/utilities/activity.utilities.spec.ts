4f3b322707d772f1bb5c433be40153c15f8517c6
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-08-13 20:03:46 +0200

    fix: tests

--- src/events/utilities/activity.utilities.spec.ts
+++ src/events/utilities/activity.utilities.spec.ts
@@ -9,8 +9,9 @@ import { EventInterface } from '../event.interface';
 import { Creator } from '../../creators/creator';
 import { ActivityTypes } from '../../activities/activity.types';
 import { Stream } from '../../streams/stream';
+import { ActivityUtilities } from './activity.utilities';
 
-describe('EventUtilities', () => {
+describe('Activity Utilities', () => {
 
   let event: EventInterface;
 
@@ -35,8 +36,8 @@ describe('EventUtilities', () => {
       new Stream(DataAltitude.type, [200, 300, 400]),
     );
 
-    expect(EventUtilities.getDataTypeMin(event.getFirstActivity(), DataHeartRate.type)).toBe(0);
-    expect(EventUtilities.getDataTypeMin(event.getFirstActivity(), DataAltitude.type)).toBe(200);
+    expect(ActivityUtilities.getDataTypeMin(event.getFirstActivity(), DataHeartRate.type)).toBe(0);
+    expect(ActivityUtilities.getDataTypeMin(event.getFirstActivity(), DataAltitude.type)).toBe(200);
   });
 
   it('should get the correct maximum for a DataType', () => {
@@ -47,8 +48,8 @@ describe('EventUtilities', () => {
       new Stream(DataAltitude.type, [200, 300, 400]),
     );
 
-    expect(EventUtilities.getDataTypeMax(event.getFirstActivity(), DataHeartRate.type)).toBe(100);
-    expect(EventUtilities.getDataTypeMax(event.getFirstActivity(), DataAltitude.type)).toBe(400);
+    expect(ActivityUtilities.getDataTypeMax(event.getFirstActivity(), DataHeartRate.type)).toBe(100);
+    expect(ActivityUtilities.getDataTypeMax(event.getFirstActivity(), DataAltitude.type)).toBe(400);
   });
 
   it('should get the correct difference for a DataType', () => {
@@ -59,8 +60,8 @@ describe('EventUtilities', () => {
       new Stream(DataAltitude.type, [200, 300, 400]),
     );
 
-    expect(EventUtilities.getDataTypeMinToMaxDifference(event.getFirstActivity(), DataHeartRate.type)).toBe(100);
-    expect(EventUtilities.getDataTypeMinToMaxDifference(event.getFirstActivity(), DataAltitude.type)).toBe(200);
+    expect(ActivityUtilities.getDataTypeMinToMaxDifference(event.getFirstActivity(), DataHeartRate.type)).toBe(100);
+    expect(ActivityUtilities.getDataTypeMinToMaxDifference(event.getFirstActivity(), DataAltitude.type)).toBe(200);
   });
 
   it('should get the correct average for a DataType', () => {
@@ -71,15 +72,15 @@ describe('EventUtilities', () => {
       new Stream(DataAltitude.type, [200, 300, 400]),
     );
 
-    expect(EventUtilities.getDataTypeAvg(event.getFirstActivity(), DataHeartRate.type)).toBe(50);
-    expect(EventUtilities.getDataTypeAvg(event.getFirstActivity(), DataAltitude.type)).toBe(300);
+    expect(ActivityUtilities.getDataTypeAvg(event.getFirstActivity(), DataHeartRate.type)).toBe(50);
+    expect(ActivityUtilities.getDataTypeAvg(event.getFirstActivity(), DataAltitude.type)).toBe(300);
   });
 
   it('should get the correct gain for a DataType', () => {
     event.getFirstActivity().addStream(
       new Stream(DataAltitude.type, [200, 300, 400]),
     );
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type)).toBe(200);
 
 
     // Add more altitude data but this time descending so it would not affect the gain
@@ -87,7 +88,7 @@ describe('EventUtilities', () => {
     event.getFirstActivity().getStreamData(DataAltitude.type).push(300);
     event.getFirstActivity().getStreamData(DataAltitude.type).push(200);
 
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type)).toBe(200);
 
     // Add more for gain
 
@@ -96,7 +97,7 @@ describe('EventUtilities', () => {
      // Gain 400
     event.getFirstActivity().getStreamData(DataAltitude.type).push(400); // Gain 500
 
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type)).toBe(500);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type)).toBe(500);
   });
 
   it('should get the correct gain for a DataType with a changed min difference', () => {
@@ -104,11 +105,11 @@ describe('EventUtilities', () => {
       new Stream(DataAltitude.type, [200, 300, 400]),
     );
     // With a diff of 100,200 the gain should be included
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 100)).toBe(200);
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 200)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 100)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 200)).toBe(200);
 
     // with a diff of 201 it shouldn't
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 201)).toBe(0);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 201)).toBe(0);
 
     // Add more
     event.getFirstActivity().getStreamData(DataAltitude.type).push(100);
@@ -116,12 +117,12 @@ describe('EventUtilities', () => {
     event.getFirstActivity().getStreamData(DataAltitude.type).push(102);
 
     // Up to now we have 200m, 300m, 400m, 100m, 101m, 102m
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 100)).toBe(200);
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 200)).toBe(200);
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 300)).toBe(0);
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 1)).toBe(202);
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 2)).toBe(202);
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 3)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 100)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 200)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 300)).toBe(0);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 1)).toBe(202);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 2)).toBe(202);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 3)).toBe(200);
   });
 
 
@@ -130,7 +131,7 @@ describe('EventUtilities', () => {
       new Stream(DataAltitude.type, [100, 300, 200, 400]),
     );
 
-    expect(EventUtilities.getEventDataTypeGain(event.getFirstActivity(), DataAltitude.type)).toBe(400);
+    expect(ActivityUtilities.getActivityDataTypeGain(event.getFirstActivity(), DataAltitude.type)).toBe(400);
   });
 
   it('should get the correct loss for a DataType', () => {
@@ -138,19 +139,19 @@ describe('EventUtilities', () => {
       new Stream(DataAltitude.type, [400, 300, 200]),
     );
 
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type)).toBe(200);
     // Add more altitude data but this time ascenting so it would not affect the Loss
     event.getFirstActivity().getStreamData(DataAltitude.type).push(200); // Loss 0
     event.getFirstActivity().getStreamData(DataAltitude.type).push(300); // Loss 0
     event.getFirstActivity().getStreamData(DataAltitude.type).push(400); // Loss 0
 
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type)).toBe(200);
 
     event.getFirstActivity().getStreamData(DataAltitude.type).push(200); // loss 200
     event.getFirstActivity().getStreamData(DataAltitude.type).push(300); // loss 0
     event.getFirstActivity().getStreamData(DataAltitude.type).push(200); // Gain 100 a total (see above of 500)
 
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type)).toBe(500);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type)).toBe(500);
   });
 
   it('should get the correct loss for a DataType with a changed min difference', () => {
@@ -159,11 +160,11 @@ describe('EventUtilities', () => {
     );
 
     // With a diff of 100,200 the gain should be included
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 100)).toBe(200);
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 200)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 100)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 200)).toBe(200);
 
     // with a diff of 201 it shouldn't
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 201)).toBe(0);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 201)).toBe(0);
 
     // Add more
     event.getFirstActivity().getStreamData(DataAltitude.type).push(500);
@@ -172,12 +173,12 @@ describe('EventUtilities', () => {
 
     // 200m, 300m, 400m, 100m, 101m, 102m
     // Up to now we have 400m, 300m, 200m, 500m, 499m, 498m
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 100)).toBe(200);
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 200)).toBe(200);
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 300)).toBe(0);
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 1)).toBe(202);
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 2)).toBe(202);
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 3)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 100)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 200)).toBe(200);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 300)).toBe(0);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 1)).toBe(202);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 2)).toBe(202);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type, void 0, void 0, 3)).toBe(200);
   });
 
 
@@ -185,7 +186,7 @@ describe('EventUtilities', () => {
     event.getFirstActivity().getAllStreams().push(
       new Stream(DataAltitude.type, [400, 200, 300, 100]), // loos 0, 200, 0, 400
     );
-    expect(EventUtilities.getEventDataTypeLoss(event.getFirstActivity(), DataAltitude.type)).toBe(400);
+    expect(ActivityUtilities.getActivityDataTypeLoss(event.getFirstActivity(), DataAltitude.type)).toBe(400);
   });
 
   it('should get the correct data length', () => {
@@ -193,16 +194,16 @@ describe('EventUtilities', () => {
     // 10 seconds = 11 slugs; from second 1 eg
     // [1,2,3,4,5,6,7,8,9,10] ->
     // [0, 1, 2,3,4,5,6,7,8,9,10]
-    expect(EventUtilities.getDataLength(activity.startDate, activity.endDate)).toBe(11);
+    expect(ActivityUtilities.getDataLength(activity.startDate, activity.endDate)).toBe(11);
     // Change start / end date to <1s
     activity.startDate = new Date(0);
     activity.endDate = new Date(50);// 50ms
     // More than 0 = 1 slug
-    expect(EventUtilities.getDataLength(activity.startDate, activity.endDate)).toBe(2);
+    expect(ActivityUtilities.getDataLength(activity.startDate, activity.endDate)).toBe(2);
     // Change start / end date to >9999ms and <100000s
     activity.startDate = new Date(0);
     activity.endDate = new Date(9999);// 9.9 seconds
     // more than 9 is 10 slugs
-    expect(EventUtilities.getDataLength(activity.startDate, activity.endDate)).toBe(11);
+    expect(ActivityUtilities.getDataLength(activity.startDate, activity.endDate)).toBe(11);
   });
 });

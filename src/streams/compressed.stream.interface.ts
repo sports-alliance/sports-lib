58bcdd7b702e014d47a0f1ea2b6fa88fe51b786a
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-09-07 19:11:27 +0200

    chore: add stream interface for compression

--- src/streams/compressed.stream.interface.ts
+++ src/streams/compressed.stream.interface.ts
@@ -1,5 +1,20 @@
-import { SerializableClassInterface } from '../serializable/serializable.class.interface';
+import { StreamJSONInterface } from './stream';
 
-export interface CompressedStreamInterface extends SerializableClassInterface {
-  compressionMethod: 'lz-string' | 'Pako' | 'Pako -> lz-string'
+export interface CompressedJSONStreamInterface extends StreamJSONInterface {
+  type: string
+  data: any
+  compressionMethod: CompressionMethods
+  encoding: CompressionEncodings
+}
+
+export enum CompressionEncodings {
+  None = 'None',
+  Binary = 'Binary',
+  base64 = 'base64',
+  UInt8 = 'Uint8',
+}
+
+export enum CompressionMethods {
+  None = 'None',
+  Pako = 'Pako',
 }

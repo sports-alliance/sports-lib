5f0df21a7074a5b419b8df340553a95d13427197
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-02-23 13:52:20 +0200

    fix: abs prerssure

--- src/data/data.absolute-pressure.ts
+++ src/data/data.absolute-pressure.ts
@@ -3,4 +3,8 @@ import { DataNumber } from './data.number';
 export class DataAbsolutePressure extends DataNumber {
   static type = 'Absolute Pressure';
   static unit = 'hpa';
+
+  getDisplayValue() {
+    return this.getValue().toFixed(1);
+  }
 }
